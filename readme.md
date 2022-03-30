14e77357fbc7450c6a0617c8a8948b2422ba636c
Bastian Burger <bastian.burger@microsoft.com>
2022-03-30 15:56:10 +0200

    Create parametrized benchmark

---
+++ readme.md
@@ -0,0 +1,2 @@
+1. Start the server with `cargo run --bin server --release`
+2. Run the benchmark with `cargo bench`
--- src/bench/main.rs
+++ src/bench/main.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use temperature::temperature_service_client::TemperatureServiceClient;
 
 pub mod temperature {
@@ -6,17 +6,28 @@ pub mod temperature {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("grpc_command", |b| {
-        b.to_async(tokio::runtime::Runtime::new().unwrap()).iter(|| async {
-            let mut client = create_grpc_client().await;
-            for _ in 0..100 {
-                client
-                    .set_temperature(temperature::RequestedTemperature { value: 10.0 })
-                    .await
-                    .unwrap();
-            }
-        })
-    });
+    let mut group = c.benchmark_group("grpc_command");
+
+    for number_of_calls in [100, 500, 1000] {
+        group.bench_with_input(
+            BenchmarkId::from_parameter(number_of_calls),
+            &number_of_calls,
+            |b, &n_calls| {
+                b.to_async(tokio::runtime::Runtime::new().unwrap())
+                    .iter(|| async {
+                        let mut client = create_grpc_client().await;
+                        for _ in 0..n_calls {
+                            client
+                                .set_temperature(temperature::RequestedTemperature { value: 10.0 })
+                                .await
+                                .unwrap();
+                        }
+                    })
+            },
+        );
+    }
+
+    group.finish();
 }
 
 async fn create_grpc_client() -> TemperatureServiceClient<tonic::transport::Channel> {
